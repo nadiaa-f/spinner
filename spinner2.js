2e44639ffe42fc00edc78d50bd480dd6d6729822
Nadia Fatah <nadi.fatah@gmail.com>
2021-12-02 14:53:51 +0000

    lint fixes

--- spinner2.js
+++ spinner2.js
@@ -1,10 +1,10 @@
 let animation = ["|", "/", "-", "\\", "\n"];
-timer = 150
-index = 0
+let timer = 150;
+
 
 for (const char in animation) {
-setTimeout(() => {
-  process.stdout.write(`\r ${animation[char]}`);
-}, timer)
-timer += 400
-};
+  setTimeout(() => {
+    process.stdout.write(`\r ${animation[char]}`);
+  }, timer);
+  timer += 400;
+}
